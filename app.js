841227e307374811e9f0ae4d6d12249a524f7850
Michał Sibik <chudymichal123@gmail.com>
2019-07-19 00:36:25 +0200

    stworzenie mapy dla kazdego pokoju gry jako klucza

--- app.js
+++ app.js
@@ -19,11 +19,12 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + `/index.html`)
 })
 
-var allClients = [];
+var allClients = []
+var gameRoom = new Map()
 
 function removeSender(sock, all){
     var newArr = Object.keys(all);
-    console.log(newArr)
+    // console.log(newArr)
     for(i=0;i<newArr.length;i++){
         newArr[i] = newArr[i].toString()
         if(newArr[i] == sock.id){
@@ -45,6 +46,18 @@ io.on('connection', (socket) => {
         socket.emit('users', removeSender(socket, io.sockets.adapter.rooms[`room-${roomNumber}`].sockets))
         io.in(`room-${roomNumber}`).emit('user', socket.id)
     }, 250)
+
+    if(gameRoom.has(`room-${roomNumber}`)){
+        var room = gameRoom.get(`room-${roomNumber}`)
+        room.clients.push(socket.id)
+        gameRoom.set(`room-${roomNumber}`, room)
+        console.log(gameRoom.get(`room-${roomNumber}`).clients)
+    }
+    else{
+        var clients = [socket.id]
+        gameRoom.set(`room-${roomNumber}`, {clients})
+        console.log(gameRoom.get(`room-${roomNumber}`).clients)
+    }
     
     if(allClients[0] == socket){
         socket.emit('side', {left: true})
@@ -76,34 +89,44 @@ io.on('connection', (socket) => {
         var i = allClients.indexOf(socket)
         allClients.splice(i, 1)
         io.emit('dconnected', socket.id)
+        var room = gameRoom.get(`room-${roomNumber}`)
+        var index = room.clients.indexOf(socket.id)
+        room.clients.splice(index, 1)
+        if(room.clients.length == 0){
+            gameRoom.delete(`room-${roomNumber}`)
+        }
+        else{
+            gameRoom.set(`room-${roomNumber}`, room)
+            console.log(gameRoom.get(`room-${roomNumber}`))
+        }
     })
 })
 
-setInterval(function(){
+// setInterval(function(){
 
-    b.hitRightPaddle(rightPaddle);
-    b.hitLeftPaddle(leftPaddle);
+//     b.hitRightPaddle(rightPaddle);
+//     b.hitLeftPaddle(leftPaddle);
 
-    if(b.update() == -1){
-        leftPaddle.reset();
-        rightPaddle.reset();
-        rightScore++;
-        b.reset();
-        }
-        else if(b.update() == 1){
-        leftScore++;
-        leftPaddle.reset();
-        rightPaddle.reset();
-        b.reset();
-        }
+//     if(b.update() == -1){
+//         leftPaddle.reset();
+//         rightPaddle.reset();
+//         rightScore++;
+//         b.reset();
+//         }
+//         else if(b.update() == 1){
+//         leftScore++;
+//         leftPaddle.reset();
+//         rightPaddle.reset();
+//         b.reset();
+//         }
 
-    io.emit('tick', {
-        x: b.x,
-        y: b.y,
-        leftScore,
-        rightScore,
-        leftPaddle,
-        rightPaddle
-    });
-}, 15);
+//     io.emit('tick', {
+//         x: b.x,
+//         y: b.y,
+//         leftScore,
+//         rightScore,
+//         leftPaddle,
+//         rightPaddle
+//     });
+// }, 15);
 server.listen(3000)
